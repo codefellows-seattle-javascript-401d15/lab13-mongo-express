3c022d76988dc1e32c83bba077728d991522c1d1
Steven Johnson <stevenjohnson86@gmail.com>
2017-05-04 17:30:02 -0700

    fixed routes and controller to use .find .update .delete. EXCELSIOR

--- lab-steven/routes/routes.js
+++ lab-steven/routes/routes.js
@@ -5,7 +5,7 @@ const hawkCtrlr = require('../controller/hawk-controller');
 
 module.exports = function(router){
   router.get('/api/hawk/:id', (req, res) => {
-    hawkCtrlr.readHawk(`{ _id : ${req.params.id}}`)
+    hawkCtrlr.readHawk({ '_id' : `${req.params.id}`})
     .then(data => res.json(data))
     .catch(err => res.status(404).send(err.message));
   });
@@ -24,13 +24,13 @@ module.exports = function(router){
   });
 
   router.put('/api/hawk/:id', (req, res) => {
-    hawkCtrlr.updateHawk(`{_id: ${req.params.id}}`, req.body)
+    hawkCtrlr.updateHawk({ '_id' : `${req.params.id}`}, req.body)
     .then(data => res.json(data))
     .catch(err => res.status(404).send(err.message));
   });
 
   router.delete('/api/hawk/:id', (req, res) => {
-    hawkCtrlr.deleteHawk(`{_id: ${req.params.id}}`)
+    hawkCtrlr.deleteHawk({ '_id' : `${req.params.id}`})
     .then(() => res.sendStatus(204))
     .catch(err => res.status(400).send(err.message));
   });
