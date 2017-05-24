31e8863bb356d92895db7a9f573f7b1e1b4cf076
meklr <mariareed@humanoid.net>
2017-05-24 00:45:26 -0700

    added readme

---
+++ lab-maria/README.md
@@ -0,0 +1,27 @@
+#About
+Small Node.js App for Codefellows Coding Bootcamp.
+
+Keep *Even Better* track of your DeathNotes and their Death Gods using MONGOdb!
+
+
+##Installation:
+
+1. clone this repository and ``cd`` into it
+2. run ``npm i``
+
+## To Use:
+1. Start the server using ``npm start`` in one terminal
+2. In another, you'll need to spin up the mongo database server
+  ⋅⋅⋅ * Ensure there is a directory named 'db' in your cloned repository
+  ⋅⋅⋅ * In your terminal, enter ``mongod --dbpath ./db``
+  ⋅⋅⋅ ⋅⋅⋅ Should you come into the 'address in use' error, ``sudo killall mongod`` should do it.
+3. 2. In yet another terminal, use [HTTPie][https://httpie.org/] to perform the following CRUD operations:
+
+* POST: ``http POST localhost:3000/api/note  owner='Light Yagami' shinigami='Ryuuk' deathCount='Over 90000'``
+  ⋅⋅⋅ When making a POST request, you should get the created DeathNote object as a response where you will find an id has been created for it
+  ⋅⋅⋅ Note that the properties "owner", "shinigami" and "deathCount" are required to successfully Post a Note.
+* GET: ``http GET localhost:3000/api/note/id ``
+  ⋅⋅⋅ Again you will get the entire body of the object you've requested
+  ⋅⋅⋅ making the same GET request without an ID will return an array of all currently existing DeathNotes
+* PUT: ``http PUT localhost:3000/api/note/id owner='Misa'``
+* DELETE: ``http DELETE localhost:3000/api/note/id``
--- lab-maria/routes/note-routes.js
+++ lab-maria/routes/note-routes.js
@@ -1,8 +1,9 @@
 'use strict';
 const Note = require('../model/deathnote');
+const List = require('../model/list');
 
 module.exports = function(router) {
-  router.get('/api/note/:id', (req, res) => {
+  router.get('/note/:id', (req, res) => {
     Note.findById(req.params.id)
       .then(note => {
         if(note.length === 0) err => res.status(404).send(err.message);
@@ -11,20 +12,19 @@ module.exports = function(router) {
       .catch(err => res.status(404).send(err.message));
   });
 
-  router.get('/api/note', (req, res) => {
+  router.get('/note', (req, res) => {
     return Note.find()
       .then(note => res.json(note))
       .catch(err => res.status(404).send(err.message));
   });
 
-  router.post('/api/note', (req, res) => {
-    new Note (req.body).save()
+  router.post('/list/:id/note', (req, res) => {
+    List.findByIdAndAddNote(req.params.listId, req.body)
       .then(note => res.json(note))
-      .catch(err => res.status(500).send(err.message));
-
+      .catch(err => res.json(404).send(err.message));
   });
 
-  router.put('/api/note/:id', (req, res) => {
+  router.put('/note/:id', (req, res) => {
     if(req.params.id) {
       Note.findById(req.params.id, function(err, note) {
         if(err) res.status(500).send(err.message);
@@ -38,10 +38,12 @@ module.exports = function(router) {
     }
   });
 
-  router.delete('/api/note/:id', (req, res) => {
+  router.delete('/note/:id', (req, res) => {
     if(!req.params.id) err => res.status(500).send(err.message);
     Note.findByIdAndRemove(req.params.id)
       .then(res.sendStatus(200))
       .catch(err => res.status(500).send(err.message));
   });
+
+  return router;
 };
